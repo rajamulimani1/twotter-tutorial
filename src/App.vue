9ab453c5bbd2dd5ad2118bb3784c87d28909c6d9
rajamulimani1 <40311216+rajamulimani1@users.noreply.github.com>
2021-02-08 22:11:31 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+ <div id="app">
+   {{user.username}} user.username
+ </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'raja mulimani',
+        firstname: 'raja',
+        lastname: 'mulimani',
+        isAdmin: true
+      }
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
